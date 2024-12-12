f27c62cf56bd7244e5e74e7d78ef5d0c7b7562d1
00007518 <00007518@uca.edu.sv>
2024-12-11 22:23:46 -0600

    erase some files and generate some news :sparkles:

---
+++ src/Components/StocksComponent/StocksComponent.jsx
@@ -0,0 +1,15 @@
+import HeaderGeneric from '../../Generics/HeaderGeneric/HeaderGeneric'
+import './StocksComponent.css'
+import NavButtonGeneric from '../../Generics/NavButtonGeneric/NavButtonGeneric'
+import inventory from '../../assets/img/inventario.png'
+import stocks from '../../assets/img/ahora-disponible.png'
+
+function StocksComponent() {
+    return (
+        <div className='stocksComponent'>
+            
+        </div>
+    )
+}
+
+export default StocksComponent
